ad35afad0cabc38aab3c6a14adf958e86cde48fb
Ron Warholic <ronwarholic@learnzillion.com>
2017-05-12 22:25:47 -0400

    Removed pycache; added views

--- bears_app/urls.py
+++ bears_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^collections/new$', views.new_collection, name='new_collection'),
 ]
--- bears_app/views.py
+++ bears_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Collection, Song
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the bears index rawrrr 🐻")
+    cols = Collection.objects.all()
+    return render(request, 'collections/index.html', {'collections': cols})
+
+def new_collection(request):
+    return render(request, 'collections/new.html', {})
