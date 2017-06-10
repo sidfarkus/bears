81f4bd8385fd0a2773d668ee1e61eae86af277fe
Ron Warholic <ronwarholic@learnzillion.com>
2017-06-09 22:11:00 -0400

    Fixed create collection to use the current user

--- bears_app/views.py
+++ bears_app/views.py
@@ -11,7 +11,6 @@ def new_collection(request):
     return render(request, 'collections/new.html', {})
 
 def create_collection(request):
-    #new_user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
-    collection = Collection(name=request.POST['name'], user=new_user)
+    collection = Collection(name=request.POST['name'], user=request.user)
     collection.save()
     return redirect('index')
