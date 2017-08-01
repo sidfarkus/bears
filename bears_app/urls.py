5cf6c6924cdc04dbca70f097365af1daac265b98
Ron Warholic <ronwarholic@learnzillion.com>
2017-07-31 22:46:16 -0400

    Adding sign up

--- bears_app/urls.py
+++ bears_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^accounts/new', views.new_user, name="new_user"),
     url(r'^collections/new$', views.new_collection, name='new_collection'),
     url(r'^collections/create$', views.create_collection, name='create_collection'),
 ]
--- bears_app/views.py
+++ bears_app/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from .models import Collection, Song
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
+    # User.objects.create_user(username='aero',
+    #                          first_name='aero',
+    #                          last_name='cool',
+    #                          email='aero@secretlair.com',
+    #                          password='aero')
     cols = Collection.objects.all()
     return render(request, 'collections/index.html', {'collections': cols})
 
@@ -14,3 +20,7 @@ def create_collection(request):
     collection = Collection(name=request.POST['name'], user=request.user)
     collection.save()
     return redirect('index')
+
+def new_user(request):
+   form = UserCreationForm()
+   return render(request, 'registration/sign_up.html', {'form': form})
