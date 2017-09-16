22cc0735f11098ef78eba2427f6656f206f4373a
Ron Warholic <ronwarholic@learnzillion.com>
2017-09-15 22:33:54 -0400

    You can add songs to a collection

--- bears_app/urls.py
+++ bears_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^$', collections.index, name='index'),
     url(r'^collections/new$', collections.new, name='new_collection'),
     url(r'^collections/create$', collections.create, name='create_collection'),
+    url(r'^collections/show/(?P<id>[0-9]+)$', collections.show, name='show_collection'),
+    url(r'^collections/add_song/(?P<id>[0-9]+)$', collections.add_song, name='add_song'),
 ]
--- bears_app/views/collections.py
+++ bears_app/views/collections.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from ..models import Collection, Song
 
 def index(request):
     cols = Collection.objects.all()
     return render(request, 'collections/index.html', {'collections': cols})
 
+def show(request, id):
+    collection = Collection.objects.get(id=id)
+    return render(request, 'collections/show.html', {'collection': collection})
+
+def add_song(request, id):
+    collection = Collection.objects.get(id=id)
+    collection.song_set.create(title=request.POST['title'],
+                               album=request.POST['album'],
+                               artist=request.POST['artist'],
+                               composer=request.POST['composer'],
+                               duration=request.POST['duration'])
+    return redirect('show_collection', id=id)
+    
+@login_required
 def new(request):
     return render(request, 'collections/new.html', {})
 
+@login_required
 def create(request):
     collection = Collection(name=request.POST['name'], user=request.user)
     collection.save()
